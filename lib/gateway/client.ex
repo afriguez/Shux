4269c10d03ab5b3eafed18832974600ef266d3f7
Fer <afriguez@gmail.com>
2022-11-12 14:40:42 -0500

    feat: add opcodes & handle :heartbeat_ack

--- lib/gateway/client.ex
+++ lib/gateway/client.ex
@@ -5,7 +5,25 @@ defmodule Shux.Gateway.Client do
 
   @gateway_url "wss://gateway.discord.gg/?v=10&encoding=json"
 
-  @opcodes %{}
+  @opcodes %{
+    # receive
+    dispatch: 0,
+    # send/receive
+    heartbeat: 1,
+    # send
+    identify: 2,
+    # send
+    resume: 6,
+    # receive
+    reconnect: 7,
+    # receive
+    invalid_session: 9,
+    # receive
+    hello: 10,
+    # receive
+    heartbeat_ack: 11
+  }
+
   def start_link(opts \\ []) do
     WebSockex.start_link(@gateway_url, __MODULE__, %{seq_num: nil, heartbeat_pid: nil}, opts)
   end
@@ -39,5 +57,10 @@ defmodule Shux.Gateway.Client do
   defp handle_operation(:hello, payload, state) do
     {:ok, pid} = Heartbeat.start_link({payload.d.heartbeat_interval, payload.s, self()})
     {:ok, %{state | heartbeat_pid: pid}}
+  end
+
+  defp handle_operation(:heartbeat_ack, _payload, state) do
+    Heartbeat.ack(state.heartbeat_pid, state.seq_num)
+    {:ok, state}
   end
 end

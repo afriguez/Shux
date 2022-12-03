0f812056746fad4766a779479237ff4c41b249b5
Fer L <afriguez@gmail.com>
2022-12-03 17:12:23 -0500

    feat: add send_message and headers

--- lib/discord/api.ex
+++ lib/discord/api.ex
@@ -3,7 +3,20 @@ defmodule Shux.Discord.Api do
 
   @endpoint "https://discord.com/api/v10"
 
+  @headers [
+    {"Authorization", "Bot "},
+    {"Content-Type", "application/json"}
+  ]
+
   def process_url(url) do
     @endpoint <> url
+  end
+
+  def send_message(ch_id, content) do
+    post(
+      "/channels/#{ch_id}/messages",
+      Poison.encode!(%{content: content}),
+      @headers
+    )
   end
 end

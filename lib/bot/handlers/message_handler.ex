480410defe900343d3c1ad875d6409313960cf4b
Fer L <afriguez@gmail.com>
2022-12-03 16:45:28 -0500

    feat: add is_command? to message_handler

--- lib/bot/handlers/message_handler.ex
+++ lib/bot/handlers/message_handler.ex
@@ -7,4 +7,8 @@ defmodule Shux.Bot.Handlers.MessageHandler do
 
   def handle(data, :edited) do
   end
+
+  def is_command?(content) do
+    String.starts_with?(content, ["shux!", "shx!", "sh!", "sx!"])
+  end
 end

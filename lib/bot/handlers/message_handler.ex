3ae7e1d64269c4fd0caa575b834b7d87f5803b2e
Fer L <afriguez@gmail.com>
2022-12-12 08:13:47 -0500

    feat: use is_command? and try to run the command if true

--- lib/bot/handlers/message_handler.ex
+++ lib/bot/handlers/message_handler.ex
@@ -1,5 +1,27 @@
 defmodule Shux.Bot.Handlers.MessageHandler do
+  @commands %{
+    help: Commands.Help
+  }
+
   def handle(data) do
+    if is_command?(data.content) do
+      [command | tail] =
+        data.content
+        |> String.split("!")
+        |> tl()
+        |> List.to_string()
+        |> String.split()
+
+      content = List.to_string(tail)
+      args = String.downcase(content) |> String.split()
+      opts = []
+
+      current_command = Map.get(@commands, command)
+
+      unless current_command == nil do
+        {_status, _message} = current_command.run(:user, content, args, opts)
+      end
+    end
   end
 
   def handle(data, :deleted) do

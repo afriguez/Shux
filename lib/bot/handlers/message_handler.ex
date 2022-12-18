03fe030936fe430ae86802097e16cb8bc1be6592
Fer L <afriguez@gmail.com>
2022-12-18 15:57:11 -0500

    feat: add parse_content to get command as atom & args as strings

--- lib/bot/handlers/message_handler.ex
+++ lib/bot/handlers/message_handler.ex
@@ -1,36 +1,42 @@
 defmodule Shux.Bot.Handlers.MessageHandler do
+  alias Shux.Bot.Commands
+
   @commands %{
     help: Commands.Help
   }
 
   def handle(data) do
-    if is_command?(data.content) do
-      [command | tail] =
-        data.content
-        |> String.split("!")
-        |> tl()
-        |> List.to_string()
-        |> String.split()
+    content = data.content
 
-      content = List.to_string(tail)
-      args = String.downcase(content) |> String.split()
-      opts = []
-
+    if is_command?(content) do
+      [command | args] = parse_content(content)
       current_command = Map.get(@commands, command)
 
       unless current_command == nil do
-        {_status, _message} = current_command.run(:user, content, args, opts)
+        # perms will be deleted after we setup the api
+        # so that we can get user permissions from there
+        perms = current_command.help().perms
+
+        {_status, _res} = current_command.run(perms, data, args)
       end
     end
   end
 
-  def handle(data, :deleted) do
+  def handle(_data, :deleted) do
   end
 
-  def handle(data, :edited) do
+  def handle(_data, :edited) do
   end
 
-  def is_command?(content) do
-    String.starts_with?(content, ["shux!", "shx!", "sh!", "sx!"])
+  def is_command?(content),
+    do: String.downcase(content) |> String.starts_with?(["shux!", "shx!", "sh!", "sx!"])
+
+  def parse_content(content) do
+    content
+    |> String.split("!")
+    |> tl()
+    |> List.to_string()
+    |> String.split()
+    |> (fn [command | args] -> [String.to_atom(command) | args] end).()
   end
 end

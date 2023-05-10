0466a41fab6956d6f3faf3e1ed0c3a9f8c6e1929
Fer L <afriguez@gmail.com>
2023-05-10 09:27:59 -0500

    feat: handle multiple interaction types

--- lib/bot/handlers/interaction_handler.ex
+++ lib/bot/handlers/interaction_handler.ex
@@ -10,7 +10,14 @@ defmodule Shux.Bot.Handlers.InteractionHandler do
   }
 
   def handle(data) do
-    interaction = Map.get(@interactions, String.to_atom(data.data.custom_id))
+    name =
+      if Map.get(data.data, :custom_id) != nil do
+        String.to_atom(data.data.custom_id)
+      else
+        String.to_atom(String.downcase(data.data.name))
+      end
+
+    interaction = Map.get(@interactions, name)
 
     unless interaction == nil do
       interaction.run(data)

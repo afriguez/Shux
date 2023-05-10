b305335659051c53d21899cc338f6756996aee95
Fer L <afriguez@gmail.com>
2023-05-10 09:30:34 -0500

    feat: respond according to the interaction type

--- lib/bot/interactions/avatar.ex
+++ lib/bot/interactions/avatar.ex
@@ -2,21 +2,71 @@ defmodule Shux.Bot.Interactions.Avatar do
   alias Shux.Bot.Components
   alias Shux.Discord.Api
 
-  def run(interaction) do
-    custom_id = interaction.data.custom_id
+  def run(%{data: %{custom_id: custom_id}} = interaction) do
     user_id = interaction.member.user.id
 
     guild_id = interaction.guild_id
     member_has_avatar = interaction.member.avatar != nil
 
-    {content, custom_id} =
+    {avatar_url, custom_id} =
       if custom_id == "avatar" and member_has_avatar do
-        {member_avatar(guild_id, user_id, interaction.member.avatar), "member_avatar"}
+        {
+          member_avatar(guild_id, user_id, interaction.member.avatar),
+          "member_avatar"
+        }
       else
-        {user_avatar(user_id, interaction.member.user.avatar), "avatar"}
+        {
+          user_avatar(user_id, interaction.member.user.avatar),
+          "avatar"
+        }
       end
 
-    components = [
+    response = %{
+      type: 7,
+      data: %{
+        content: avatar_url,
+        components: components(custom_id, avatar_url)
+      }
+    }
+
+    Api.interaction_callback(interaction, response)
+  end
+
+  def run(interaction) do
+    has_resolved = Map.get(interaction.data, :resolved) != nil
+
+    {user, member} =
+      if has_resolved do
+        key = hd(Map.keys(interaction.data.resolved.users))
+
+        {
+          Map.get(interaction.data.resolved.users, key),
+          Map.get(interaction.data.resolved.members, key)
+        }
+      else
+        {interaction.member.user, interaction.member}
+      end
+
+    avatar_url =
+      if member.avatar != nil do
+        member_avatar(interaction.data.guild_id, user.id, member.avatar)
+      else
+        user_avatar(user.id, user.avatar)
+      end
+
+    response = %{
+      type: 4,
+      data: %{
+        content: avatar_url,
+        components: components("member_avatar", avatar_url)
+      }
+    }
+
+    Api.interaction_callback(interaction, response)
+  end
+
+  def components(custom_id, avatar_url) do
+    [
       Components.action_row([
         Components.button(
           style: 1,
@@ -30,31 +80,21 @@ defmodule Shux.Bot.Interactions.Avatar do
         Components.button(
           style: 5,
           label: "Abrir original",
-          url: content
+          url: avatar_url
         )
       ])
     ]
-
-    response = %{
-      type: 7,
-      data: %{
-        content: content,
-        components: components
-      }
-    }
-
-    Api.interaction_callback(interaction, response)
   end
 
   def member_avatar(guild_id, user_id, avatar) do
     "https://cdn.discordapp.com/guilds/" <>
       "#{guild_id}/users/#{user_id}/avatars/" <>
-      "#{avatar <> avatar_ext(avatar)}?size=1024"
+      "#{avatar <> avatar_ext(avatar)}?size=2048"
   end
 
   def user_avatar(user_id, avatar) do
     "https://cdn.discordapp.com/avatars/" <>
-      "#{user_id}/#{avatar <> avatar_ext(avatar)}?size=1024"
+      "#{user_id}/#{avatar <> avatar_ext(avatar)}?size=2048"
   end
 
   def avatar_ext("a_" <> _avatar), do: ".gif"

4c03b62739f67df952997f2a23c96530086a3668
Fer L. <afriguez@gmail.com>
2023-10-12 10:05:04 -0500

    feat: use pattern matching to close tickets

--- lib/bot/interactions/ticket.ex
+++ lib/bot/interactions/ticket.ex
@@ -4,71 +4,70 @@ defmodule Shux.Bot.Interactions.Ticket do
   alias Shux.Bot.Components
   alias Shux.Discord.Api
 
+  def run(%{data: %{custom_id: "close_ticket"}} = interaction),
+    do: Api.delete_channel(interaction.channel_id)
+
   def run(interaction) do
-    if interaction.data.custom_id == "close_ticket" do
-      Api.delete_channel(interaction.channel_id)
-    else
-      {:ok, %{body: body}} =
-        Api.create_channel(interaction.guild_id, %{
-          name: interaction.member.user.id,
-          type: 0,
-          permission_overwrites: [
-            %{
-              id: interaction.member.user.id,
-              type: 1,
-              allow: Integer.to_string(0x400 ||| 0x800 ||| 0x4000 ||| 0x8000 ||| 0x10000)
-            }
-          ]
-        })
-
-      {:ok, %{id: channel_id}} = Poison.decode(body, %{keys: :atoms})
-
-      Api.send_message(channel_id, %{
-        content: "<@#{interaction.member.user.id}>",
-        embeds: [
+    {:ok, %{body: body}} =
+      Api.create_channel(interaction.guild_id, %{
+        name: interaction.member.user.id,
+        type: 0,
+        permission_overwrites: [
           %{
-            description:
-              "Recuerde enviar directamente el problema y specs del dispositivo.\n" <>
-                "Si necesite ayuda con un presupuesto, especifique la moneda y el país\n\n" <>
-                "*Por favor espere a que un técnico o administrador este disponible.*",
-            color: random_color()
+            id: interaction.member.user.id,
+            type: 1,
+            allow: Integer.to_string(0x400 ||| 0x800 ||| 0x4000 ||| 0x8000 ||| 0x10000)
           }
-        ],
-        components: [
-          Components.action_row([
-            Components.button(
-              style: 4,
-              label: "Cerrar ticket",
-              emoji: %{name: "🎫", id: nil},
-              custom_id: "close_ticket"
-            )
-          ])
         ]
       })
 
-      response =
-        cond do
-          interaction.data.custom_id == "persistent_ticket" ->
-            %{
-              type: 4,
-              data: %{
-                content: "**Ticket abierto:** <##{channel_id}>",
-                flags: Integer.to_string(0x40)
-              }
-            }
+    {:ok, %{id: channel_id}} = Poison.decode(body, %{keys: :atoms})
 
-          true ->
-            %{
-              type: 7,
-              data: %{
-                content: "**Ticket abierto:** <##{channel_id}>",
-                components: []
-              }
-            }
-        end
+    Api.send_message(channel_id, %{
+      content: "<@#{interaction.member.user.id}>",
+      embeds: [
+        %{
+          description:
+            "Recuerde enviar directamente el problema y specs del dispositivo.\n" <>
+              "Si necesita ayuda con un presupuesto, especifique la moneda y el país\n\n" <>
+              "*Por favor espere a que un técnico o administrador este disponible.*",
+          color: random_color()
+        }
+      ],
+      components: [
+        Components.action_row([
+          Components.button(
+            style: 4,
+            label: "Cerrar ticket",
+            emoji: %{name: "🎫", id: nil},
+            custom_id: "close_ticket"
+          )
+        ])
+      ]
+    })
 
-      Api.interaction_callback(interaction, response)
-    end
+    response =
+      cond do
+        interaction.data.custom_id == "persistent_ticket" ->
+          %{
+            type: 4,
+            data: %{
+              content: "**Ticket abierto:** <##{channel_id}>",
+              flags: Integer.to_string(0x40)
+            }
+          }
+
+        true ->
+          %{
+            type: 7,
+            data: %{
+              content: "**Ticket abierto:** <##{channel_id}>",
+              components: []
+            }
+          }
+      end
+
+    Api.interaction_callback(interaction, response)
   end
 
   def random_color, do: :math.floor(:rand.uniform() * 0xFFFFFF)

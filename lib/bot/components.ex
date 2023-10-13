0e08f91c6dd9c679da91564d694068f7d9e91136
Fer L. <afriguez@gmail.com>
2023-10-13 14:18:41 -0500

    feat: add string_select and string_options components

--- lib/bot/components.ex
+++ lib/bot/components.ex
@@ -14,4 +14,22 @@ defmodule Shux.Bot.Components do
       disabled: Keyword.get(props, :disabled, nil)
     }
   end
+
+  def string_select(props \\ []) do
+    %{
+      type: 3,
+      custom_id: Keyword.get(props, :custom_id, nil),
+      options: Keyword.get(props, :options, nil),
+      placeholder: Keyword.get(props, :placeholder, nil)
+    }
+  end
+
+  def string_option(props \\ []) do
+    %{
+      label: Keyword.get(props, :label, nil),
+      value: Keyword.get(props, :label, nil),
+      description: Keyword.get(props, :label, nil),
+      emoji: Keyword.get(props, :emoji, nil)
+    }
+  end
 end

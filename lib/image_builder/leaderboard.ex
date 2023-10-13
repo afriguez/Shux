bbe5047fc171c0ed34a7caad71e0fe27db7877fc
Adrephos <adrephos@gmail.com>
2023-10-13 18:39:25 -0500

    feat: add text composition to leaderboard

--- lib/image_builder/leaderboard.ex
+++ lib/image_builder/leaderboard.ex
@@ -4,6 +4,7 @@ defmodule Shux.ImageBuilder.Leaderboard do
     |> get_avatars()
     |> compose_leaderboard()
     |> compose_text(users)
+    |> Image.write!(:memory, suffix: ".png")
   end
 
   defp get_avatars(users) do
@@ -36,8 +37,70 @@ defmodule Shux.ImageBuilder.Leaderboard do
     |> Image.compose!(leaderboard_bg)
   end
 
-  defp compose_text(leaderboard, _users) do
-    Image.write!(leaderboard, :memory, suffix: ".png")
+  defp make_texts(users) do
+    # { t_username, t_points, t_level }
+    font = "Poppins"
+
+    texts_pos = [
+      {303, 148},
+      {121, 205},
+      {481, 205},
+      {139, 322},
+      {377, 322}
+    ]
+
+    Enum.zip(users, texts_pos)
+    |> Enum.with_index()
+    |> Enum.reduce([], fn {{{_, username, points, level}, {x, y}}, i}, acc ->
+      font_size = if i <= 2, do: 18, else: 14
+
+      [
+        {Image.Text.text!(
+           username,
+           font: font,
+           font_size: font_size,
+           font_weight: 600
+         ),
+         Image.Text.text!(
+           "Puntos: #{points}",
+           font: font,
+           font_size: 10,
+           font_weight: 600
+         ),
+         Image.Text.text!(
+           "Nivel: #{level}",
+           font: font,
+           font_size: 10,
+           font_weight: 600
+         ), x, y}
+        | acc
+      ]
+    end)
+    |> Enum.reverse()
+  end
+
+  defp compose_text(leaderboard, users) do
+    make_texts(users)
+    |> Enum.with_index()
+    |> Enum.reduce(leaderboard, fn {{t_username, t_points, t_level, x, y}, i}, acc ->
+      h_username = Image.height(t_username) + 5
+      h_points = Image.height(t_points) + h_username + 5
+
+      {x_u, x_p, x_l} =
+        if i <= 2 do
+          {
+            (x - Image.width(t_username) / 2) |> floor(),
+            (x - Image.width(t_points) / 2) |> floor(),
+            (x - Image.width(t_level) / 2) |> floor()
+          }
+        else
+          {x, x, x}
+        end
+
+      Image.compose!(acc, t_username, x: x_u, y: y)
+      |> Image.compose!(t_points, x: x_p, y: y + h_username)
+      |> Image.compose!(t_level, x: x_l, y: y + h_points)
+    end)
   end
 
   defp compose_avatars([], img), do: img

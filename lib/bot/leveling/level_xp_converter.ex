2b7bdb3f7066b2cfd5f881d06857b5a66862e84a
Adrephos <adrephos@gmail.com>
2023-10-13 14:04:33 -0500

    feat: add level percentage definition

--- lib/bot/leveling/level_xp_converter.ex
+++ lib/bot/leveling/level_xp_converter.ex
@@ -1,12 +1,17 @@
 defmodule Shux.Bot.Leveling.LevelXpConverter do
   def xp_to_level(xp) do
     (2.5 * :math.sqrt(xp))
-    |> round()
+    |> floor()
   end
 
   def level_to_xp(level) do
     (level / 2.5)
     |> :math.pow(2)
     |> round()
+  end
+
+  def level_percentage(xp) do
+    (2.5 * :math.sqrt(xp) - xp_to_level(xp))
+    |> Float.round(2)
   end
 end

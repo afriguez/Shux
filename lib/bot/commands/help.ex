3d8fa7daea1151f445045f4ab9247b060b1a74af
Fer L. <afriguez@gmail.com>
2023-11-04 10:38:53 -0500

    feat: update help command

--- lib/bot/commands/help.ex
+++ lib/bot/commands/help.ex
@@ -18,28 +18,27 @@ defmodule Shux.Bot.Commands.Help do
     process(msg, args, commands)
   end
 
-  def process(msg, [], commands) do
-    list =
-      Enum.map(commands, fn {k, v} ->
-        %{
-          name: Atom.to_string(k),
-          value: v.help().description, inline: true
-        }
-      end)
+  defp process(msg, [], commands) do
+    command_list =
+      Enum.sort_by(commands, fn {k, _v} -> Atom.to_string(k) end)
+
+    slash_commands = Api.global_commands()
+
+    fields = build_fields(command_list, slash_commands)
 
     Api.send_message(msg.channel_id, %{
       embeds: [
         %{
           title: "Ayuda Shux",
-          description: help().description,
+          description: "Comandos de Shuxbot",
           color: :math.floor(:rand.uniform() * 0xFFFFFF),
-          fields: list
+          fields: fields
         }
       ]
     })
   end
 
-  def process(msg, args, commands) do
+  defp process(msg, args, commands) do
     command = String.to_atom(hd(args))
 
     current_command = Map.get(commands, command)
@@ -47,16 +46,23 @@ defmodule Shux.Bot.Commands.Help do
     unless current_command == nil do
       help = current_command.help()
 
+      [slash_cmd | _] = Api.global_commands() |> Enum.filter(&(&1.name == hd(args)))
+
       Api.send_message(msg.channel_id, %{
         embeds: [
           %{
-            title: "Ayuda commando: " <> hd(args),
+            title: "Ayuda comando: " <> hd(args),
             description: help.description,
             color: :math.floor(:rand.uniform() * 0xFFFFFF),
             fields: [
               %{
                 name: "Uso: ",
                 value: help.usage,
+                inline: true
+              },
+              %{
+                name: "Slash: ",
+                value: "</#{slash_cmd.name}:#{slash_cmd.id}>",
                 inline: true
               }
             ]
@@ -65,4 +71,22 @@ defmodule Shux.Bot.Commands.Help do
       })
     end
   end
+
+  defp build_fields(command_list, slash_commands) do
+    for {k, v} <- command_list do
+      cmd = Atom.to_string(k)
+      filtered_cmds = Enum.filter(slash_commands, &(&1.name == cmd))
+
+      slash_string = build_slash_string(filtered_cmds)
+
+      %{
+        name: "#{cmd}" <> slash_string,
+        value: v.help().description,
+        inline: true
+      }
+    end
+  end
+
+  defp build_slash_string([]), do: ""
+  defp build_slash_string([slash_cmd | _]), do: " - </#{slash_cmd.name}:#{slash_cmd.id}>"
 end

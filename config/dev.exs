42cabbffe952d300e974ab632cdb4d45d3926817
Fer L. <afriguez@gmail.com>
2023-12-14 16:50:20 -0500

    feat: add environment variables to config files

--- config/dev.exs
+++ config/dev.exs
@@ -2,4 +2,7 @@ import Config
 
 config :shux,
   bot_token: System.get_env("SHUX_TOKEN"),
-  app_id: System.get_env("SHUX_APP_ID")
+  app_id: System.get_env("SHUX_APP_ID"),
+  api_token: System.get_env("SHUX_API_TOKEN"),
+  username: System.get_env("SHUX_USERNAME"),
+  password: System.get_env("SHUX_PASSWORD")

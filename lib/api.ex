d007d9f8324c1e3e9054592e3e18b76235503f33
Fer L. <afriguez@gmail.com>
2023-12-21 17:46:36 -0500

    feat: remove initial_headers

--- lib/api.ex
+++ lib/api.ex
@@ -29,13 +29,6 @@ defmodule Shux.Api do
 
   def process_url(url), do: @endpoint <> url
 
-  def initial_headers do
-    [
-      {"Authorization", "Bearer " <> Application.get_env(:shux, :api_token)},
-      {"Content-Type", "application/json"}
-    ]
-  end
-
   def headers do
     {access, _refresh} = Cache.get_tokens()
 
@@ -50,7 +43,7 @@ defmodule Shux.Api do
     username = Application.get_env(:shux, :username) |> parse_env()
     post_body = %{username: username, password: password} |> Poison.encode!()
 
-    %HTTPoison.Response{body: res_body} = post!("/auth/login", post_body, initial_headers())
+    %HTTPoison.Response{body: res_body} = post!("/auth/login", post_body)
     %{"accessToken" => access, "refreshToken" => refresh} = (res_body |> Poison.decode!())["data"]
 
     {access, refresh}
@@ -58,7 +51,7 @@ defmodule Shux.Api do
 
   def refresh(token) do
     post_body = %{token: token} |> Poison.encode!()
-    %HTTPoison.Response{body: res_body} = post!("/auth/refresh", post_body, initial_headers())
+    %HTTPoison.Response{body: res_body} = post!("/auth/refresh", post_body)
     %{"accessToken" => access, "refreshToken" => refresh} = (res_body |> Poison.decode!())["data"]
 
     {access, refresh}

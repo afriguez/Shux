785889d24d0d79c8c78f324ac2d81aee9514689e
Fer L. <afriguez@gmail.com>
2023-12-21 08:50:02 -0500

    feat: correct command separation based on user permissions

--- lib/bot/commands/help.ex
+++ lib/bot/commands/help.ex
@@ -8,23 +8,23 @@ defmodule Shux.Bot.Commands.Help do
     %{
       usage: "sx!help comando",
       description: "Muestra ayuda de un comando",
-      perms: :tech,
+      perms: :user,
       options: ""
     }
   end
 
-  def run(_perms, msg, args) do
+  def run(perms, msg, args) do
     commands = MessageHandler.get_commands()
-    process(msg, args, commands)
+    process(msg, args, commands, perms)
   end
 
-  defp process(msg, [], commands) do
+  defp process(msg, [], commands, perms) do
     command_list =
       Enum.sort_by(commands, fn {k, _v} -> Atom.to_string(k) end)
 
     slash_commands = Api.global_commands()
 
-    fields = build_fields(command_list, slash_commands)
+    fields = build_fields(command_list, slash_commands, perms)
 
     Api.send_message(msg.channel_id, %{
       embeds: [
@@ -38,23 +38,30 @@ defmodule Shux.Bot.Commands.Help do
     })
   end
 
-  defp process(msg, args, commands) do
+  defp process(msg, args, commands, perms) do
     command = String.to_atom(hd(args))
 
     current_command = Map.get(commands, command)
 
     unless current_command == nil do
-      help = current_command.help()
+      role_flags = Shux.Api.get_role_flags()
+      user_flags = role_flags[perms]
+      flags = role_flags[current_command.help().perms]
 
-      [slash_cmd | _] = Api.global_commands() |> Enum.filter(&(&1.name == hd(args)))
+      if flags >= user_flags do
+        help = current_command.help()
+        global = Api.global_commands() |> Enum.filter(&(&1.name == hd(args)))
 
-      Api.send_message(msg.channel_id, %{
-        embeds: [
-          %{
-            title: "Ayuda comando: " <> hd(args),
-            description: help.description,
-            color: :math.floor(:rand.uniform() * 0xFFFFFF),
-            fields: [
+        fields =
+          if Enum.empty?(global),
+            do: [
+              %{
+                name: "Uso: ",
+                value: help.usage,
+                inline: true
+              }
+            ],
+            else: [
               %{
                 name: "Uso: ",
                 value: help.usage,
@@ -62,29 +69,50 @@ defmodule Shux.Bot.Commands.Help do
               },
               %{
                 name: "Slash: ",
-                value: "</#{slash_cmd.name}:#{slash_cmd.id}>",
+                value: "</#{hd(global).name}:#{hd(global).id}>",
                 inline: true
               }
             ]
-          }
-        ]
-      })
+
+        Api.send_message(msg.channel_id, %{
+          embeds: [
+            %{
+              title: "Ayuda comando: " <> hd(args),
+              description: help.description,
+              color: :math.floor(:rand.uniform() * 0xFFFFFF),
+              fields: fields
+            }
+          ]
+        })
+      else
+        {:invalid, "Not authorized"}
+      end
     end
   end
 
-  defp build_fields(command_list, slash_commands) do
+  defp build_fields(command_list, slash_commands, perms) do
+    role_flags = Shux.Api.get_role_flags()
+    user_flags = role_flags[perms]
+
     for {k, v} <- command_list do
+      flags = role_flags[v.help().perms]
       cmd = Atom.to_string(k)
-      filtered_cmds = Enum.filter(slash_commands, &(&1.name == cmd))
 
-      slash_string = build_slash_string(filtered_cmds)
+      if flags >= user_flags do
+        filtered_cmds = Enum.filter(slash_commands, &(&1.name == cmd))
 
-      %{
-        name: "#{cmd}" <> slash_string,
-        value: v.help().description,
-        inline: true
-      }
+        slash_string = build_slash_string(filtered_cmds)
+
+        %{
+          name: "#{cmd}" <> slash_string,
+          value: v.help().description,
+          inline: true
+        }
+      else
+        nil
+      end
     end
+    |> Enum.filter(&(&1 != nil))
   end
 
   defp build_slash_string([]), do: ""
